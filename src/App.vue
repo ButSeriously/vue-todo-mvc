8bc9bff9a6009336bac0e72d421630261ba0f3e9
Mateusz Bielak <mt.bielak@gmail.com>
2017-12-01 15:52:55 +0100

    Vue app logic

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <ul v-for="todo in $data.todos.filter(shouldShowTodo)">
+      <li v-bind:class="{complete: todo.done}">
+        {{ todo.text }}
+        <input type="checkbox" v-model="todo.done">
+      </li>
+    </ul>
+    <form v-on:submit="handleAddTodo($event)">
+      <input type="text" v-model="input">
+    </form>
+    <label class="show-completed">
+      Show Completed Items?
+      <input type="checkbox" v-model="showDone">
+    </label>
+  </div>  
+</template>
+<script>
+  export default {
+    methods: {
+      handleAddTodo(e) {
+        e.preventDefault();
+        this.$data.todos.push({text:this.input});
+        this.input = "";
+      },
+      shouldShowTodo(todo) {
+        if (this.$data.showDone) {
+          return true;
+        }
+        return !todo.done;
+      }
+    },
+    data(){
+      return this.$props.state;
+    },
+    props: ['state']
+  }
+</script>
+<style>
+  .complete {
+    color: gainsboro;
+    text-decoration: line-through;
+    font-style: italic;
+  }
+</style>
+
--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log('Hello world!');
+import Vue from 'vue';
+import App from './App.vue';
+
+new Vue({
+  el: '#app',
+  components: { App },
+  template: `<App :state="state"/>`,
+  data: {
+    state: {
+      showDone: false,
+      todos: [{
+        text: 'Get a job',
+        done: false,
+      }, {
+        text: 'Find a girl',
+        done: true,
+      }, {
+        text: 'Buy a car',
+        done: false,
+      }]
+    }
+  }
+})
